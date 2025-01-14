c116ec332b19ba628567253cd954586666fb7be2
santiagoquilindo <santiagoquilindo32@gmail.com>
2025-01-14 17:20:53 -0500

    enero2025

---
+++ script.js
@@ -0,0 +1,5 @@
+// Ejemplo: Mostrar alerta al enviar el formulario
+document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('¡Gracias por contactarnos! Responderemos lo antes posible.');
+});
